27b27b20e438ed5c9f1f0848ff5ce6022c5c2f57
Aloïs Colléaux-Le Chêne <alois.colleaux-le-chene@epita.fr>
2023-06-13 22:43:50 +0200

    feat: add invalid project

---
+++ maven_project_invalid/src/main/java/coucou/App.java
@@ -0,0 +1,14 @@
+package coucou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	    testsetg sdlkfjsd;lfj
+        System.out.println( "Hello World!" );
+    }
+}
